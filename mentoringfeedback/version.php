996bc10109e2f7ed7106c0397cf87e1b164ecc21
Eric Bernardo <eric.bernardo.sousa@gmail.com>
2020-04-03 14:10:23 -0300

    *

--- mentoringfeedback/version.php
+++ mentoringfeedback/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_mentoringfeedback';
-$plugin->release = '0.1.0';
-$plugin->version = 2020032500;
+$plugin->release = '0.1.1';
+$plugin->version = 2020040300;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_ALPHA;
